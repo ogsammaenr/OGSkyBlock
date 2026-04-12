102f096e3cde8c3192c0597e67fc36c54eb6eeb1
ogsammaenr <osmangamer152@gmail.com>
2026-04-12 23:45:16 +0300

    level modul yapisi eklendi

---
+++ skyblock-leveling/src/main/java/me/ogsammaenr/skyblockLevel/SkyblockLevel.java
@@ -0,0 +1,10 @@
+package me.ogsammaenr.skyblockLevel;
+
+import net.fabricmc.api.ModInitializer;
+
+public class SkyblockLevel implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
